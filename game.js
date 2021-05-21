1e7ed1a6775e76fdd70d3ad7010858f74ba9f9d3
narcisooliveira <nRcsfo@gmail.com>
2021-05-21 09:58:26 -0300

    update

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function computerPlay(){
     var array = ['Rock', 'Paper', 'Scissors'];
-    let choose = Math.floor((Math.random() * 3) + 0);
+    let choose = Math.floor((Math.random() * 3));
     return array[choose];
 }
 
@@ -13,36 +13,38 @@ function playRound(playerSelection, computerSelection){
     word = word.charAt(0).toUpperCase() + word.slice(1)
     if (word === 'Rock' && computerSelection === 'Paper'){
             machine++;
-        return "You lose! Paper beats Rock" 
+            return "You lose! Paper beats Rock" 
     }else if (word === 'Paper' && computerSelection === 'Rock'){
             human++;
-        return "You win! Paper beats Rock"
+            return "You win! Paper beats Rock"
     }else if (word === 'Scissors' && computerSelection === 'Rock'){
             machine++;
-        return "You lose! Rock beats Scissors" 
+            return "You lose! Rock beats Scissors" 
     }else if (word === 'Rock' && computerSelection === 'Scissors'){
             human++;
-        return "You win! Rock beats Scissors" 
+            return "You win! Rock beats Scissors" 
     }else if (word === 'Paper' && computerSelection === 'Scissors'){
             machine++;
-        return "You lose! Scissors beats Paper" 
-    }else (word === 'Scissors' && computerSelection === 'Paper')
+            return "You lose! Scissors beats Paper" 
+    }else if (word === 'Scissors' && computerSelection === 'Paper')
             human++;
-        return "You win! Scissors beats Paper"
+            return "You win! Scissors beats Paper"
 }
 
 function game(){
     var playerSelection;
+    var computerSelection;
     for (let i = 0; i <= 4; i++){
+        computerSelection = computerPlay();
         playerSelection = prompt("Type: Rock, Paper or Scissors: ")
         console.log(playRound(playerSelection, computerSelection));
     }
     if (human > machine)
-        console.log('The winner is human ='+human+"-"+machine)
-    else if (human > machine)
-        console.log('The winner is machine ='+human+"-"+machine)
+        console.log('The winner is human: '+human+"-"+machine)
+    else if (human < machine)
+        console.log('The winner is machine: '+machine+"-"+human)
     else 
-        console.log('Draw game ='+human+"-"+machine)
+        console.log('Draw game: '+human+"-"+machine)
 }
 
 const computerSelection = computerPlay();
