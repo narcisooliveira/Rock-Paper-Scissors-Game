3bc098f6b03a980214831313a2012f89529dc9bf
narcisooliveira <nrcsfo@gmail.com>
2021-07-03 11:37:52 -0300

    code upgrade

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Rock-Paper-Scissors-Game
-Learning javascript with an simple game
+** Learning javascript with an simple game! **
+
+-> O objetivo desse jogo é mostrar de maneira simples e intuitiva tudo que aprendi até o momento estudando javascript, e nada mais prazeroso do que programar um pequeno jogo.
+
+-> Nesse jogo o player (usuário) terá o desafio de enfrentar a máquina (computador) porém fora do escopo de machine learning (não faz sentido aplicar), o computador foi programado para selecionar de maneira randômica sua escolha, e baseado-se na escolha que o player (usuário) fez e como o game "rock-paper-scissors" funciona,
+o game irá comparar as duas escolhas e determinar o vencedor, o jogo poderá ser em rodada única ou conjunto de rodadas.
+
+-> Esse jogo servirá também como método de avaliação das minhas habilidade no tocante a alguns conceitos estudados em sala de aula como: estruturas de controle e repetição, array, functions bult-in with javascript, construções de funções próprias e entendimento de programação estruturada.
--- game.js
+++ game.js
@@ -4,6 +4,7 @@ function computerPlay(){
     return array[choose];
 }
 
+const prompt = require('prompt-sync')({sigint:true})
 var human = 0;
 var machine = 0;
 
@@ -12,41 +13,40 @@ function playRound(playerSelection, computerSelection){
     word = word.toLowerCase()
     word = word.charAt(0).toUpperCase() + word.slice(1)
     if (word === 'Rock' && computerSelection === 'Paper'){
-            machine++;
-            return "You lose! Paper beats Rock" 
+        machine++;
+        return "You lose! Paper beats Rock" 
     }else if (word === 'Paper' && computerSelection === 'Rock'){
-            human++;
-            return "You win! Paper beats Rock"
+        human++;
+        return "You win! Paper beats Rock"
     }else if (word === 'Scissors' && computerSelection === 'Rock'){
-            machine++;
-            return "You lose! Rock beats Scissors" 
+        machine++;
+        return "You lose! Rock beats Scissors" 
     }else if (word === 'Rock' && computerSelection === 'Scissors'){
-            human++;
-            return "You win! Rock beats Scissors" 
+        human++;
+        return "You win! Rock beats Scissors" 
     }else if (word === 'Paper' && computerSelection === 'Scissors'){
-            machine++;
-            return "You lose! Scissors beats Paper" 
-    }else if (word === 'Scissors' && computerSelection === 'Paper')
-            human++;
-            return "You win! Scissors beats Paper"
+        machine++;
+        return "You lose! Scissors beats Paper" 
+    }else {
+        human++;
+        return "You win! Scissors beats Paper"
+        }
 }
 
 function game(){
-    var playerSelection;
-    var computerSelection;
-    for (let i = 0; i <= 4; i++){
+    let playerSelection;
+    let computerSelection;
+    let qtdPartidas = parseInt(prompt('Insira a quantidade de partidas que deseja jogar: '))
+    for (let i = 0; i < qtdPartidas; i++){
         computerSelection = computerPlay();
         playerSelection = prompt("Type: Rock, Paper or Scissors: ")
         console.log(playRound(playerSelection, computerSelection));
     }
-    if (human > machine)
+    if (human > machine){
         console.log('The winner is human: '+human+"-"+machine)
-    else if (human < machine)
+    }else if (human < machine){
         console.log('The winner is machine: '+machine+"-"+human)
-    else 
+    }else 
         console.log('Draw game: '+human+"-"+machine)
 }
-
-const computerSelection = computerPlay();
-
 console.log(game())
